5f9667b47064d4ca538a0939518494358a176519
Amber Conville <crebma@gmail.com>
2010-11-06 08:56:19 -0400

    THIS one makes a bit more sense.

--- board.clj
+++ board.clj
@@ -1,17 +1,14 @@
 (use 'clojure.contrib.test-is)
 
+(defn make-board
+  ([size]
+    '('(:dead))
+  )
+)
+
 (deftest make-board-only-includes-possible-neighbors
- (is (= (make-board 3 3) [
-     [:dead :dead :dead]
-     [:dead :dead :dead :dead :dead]
-     [:dead :dead :dead]
-     [:dead :dead :dead :dead :dead]
-     [:dead :dead :dead :dead :dead :dead :dead :dead :dead]
-     [:dead :dead :dead :dead :dead]
-     [:dead :dead :dead]
-     [:dead :dead :dead :dead :dead]
-     [:dead :dead :dead]
-     ]))
+ (is (= (make-board 1) '('(:dead))))
+ (is (= (make-board 2) '('(:dead :dead :dead)'(:dead :dead :dead)'(:dead :dead :dead)'(:dead :dead :dead))))
 )
 
 (run-tests)
