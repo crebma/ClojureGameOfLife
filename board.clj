2d3812379b72fee23cc704d3a54fccf87dcdc03e
Amber Conville <crebma@gmail.com>
2010-11-06 13:30:24 -0400

    newest attempt

--- board.clj
+++ board.clj
@@ -1,14 +1,20 @@
 (use 'clojure.contrib.test-is)
 
+(defn make-neighbors
+  ([size]
+    (list (for [neighbors (range size)] :dead))
+  )
+)
+
 (defn make-board
   ([size]
-    '('(:dead))
+    (list (for [cells (range (* size size))] (make-neighbors size)))
   )
 )
 
 (deftest make-board-only-includes-possible-neighbors
- (is (= (make-board 1) '('(:dead))))
- (is (= (make-board 2) '('(:dead :dead :dead)'(:dead :dead :dead)'(:dead :dead :dead)'(:dead :dead :dead))))
+ (is (= (make-board 1) (list (list :dead))))
+ (is (= (make-board 2) (list (list :dead :dead) (list :dead :dead) (list :dead :dead) (list :dead :dead))))
 )
 
 (run-tests)
